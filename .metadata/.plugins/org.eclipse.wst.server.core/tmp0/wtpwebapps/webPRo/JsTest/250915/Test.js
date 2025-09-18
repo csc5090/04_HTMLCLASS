1e418c23ad574552216a4cf911aa31e89e88fb01
csc5090 <tkdcjf5090@gmail.com>
2025-09-18 10:30:37 +0900

    Initial commit

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/webPRo/JsTest/250915/Test.js
@@ -0,0 +1,13 @@
+/** 2025-09-15
+ *  개발자 : 최상철
+ * 
+ *  이 문서는 자바스크립트 명령을 저장한 파일입니다.
+ *  외부 자바스크립트 파일의 확장자는 .js 
+ * 
+ */
+
+document.write("<hr color='red'>");
+document.write("이 내용은 외부 스크립트 파일에서 출력하는 내용임.");
+document.write("출력이 잘 되는지 확인 중. <br>");
+document.write("<hr color='red'>");
+
